b508093e2b5fa5c0c91b510414c984e84a72d1a9
PotatoDoge1 <ryan.a.schwark@gmail.com>
2025-01-27 20:48:46 -0500

    updating tab name and 'book' link to project link

--- server2/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, request
-from app.controllers import signup_user
-
-routes = Blueprint("routes", __name__)
-
-@routes.route('/signup', methods=['POST'])
-def signup():
-    data = request.json
-    return signup_user(data),201
