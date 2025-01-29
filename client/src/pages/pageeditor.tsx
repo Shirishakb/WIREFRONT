122b61b01c69bf642d92caf0fdb39422d7319d84
ShirishaKb <shirishakb@gmail.com>
2025-01-29 15:13:19 -0500

    updated with latest changes

--- client/src/pages/pageeditor.tsx
+++ client/src/pages/pageeditor.tsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react';
 import { Rnd } from 'react-rnd'; // Using react-rnd for resizing and dragging
 import { useParams } from 'react-router-dom';
+import { getPageById,updatePage } from '../api/pages';
 import { getComponents } from '../api/components';
 import { COMPONENT_TYPES } from '../constants';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 
 interface Component {
@@ -17,16 +23,48 @@ const DEFAULT_POSITION_OFFSET = 50; // Offset for new components to avoid overla
 
 const PageEditor = () => {
   const { pageId } = useParams();
+  const [page, setPage] = useState<{ name: string; _id: string } | null>(null);
+  const [newPageName, setNewPageName] = useState<string>('');
+  const [isEditingPageName, setIsEditingPageName] = useState<boolean>(false);
+  const navigate = useNavigate();
   const [components, setComponents] = useState<Component[]>([]); // Components in the workspace
   const [removedComponents, setRemovedComponents] = useState<Component[]>([]); // Removed components
   const [newComponentType, setNewComponentType] = useState(COMPONENT_TYPES.BUTTON); // Default type
   const [chosenComponents, setChosenComponents] = useState<Component[]>([]); // Chosen components
   const [isEditing, setIsEditing] = useState<string | null>(null); // Track the component being edited
+  const [showModal, setShowModal] = useState(false); // To control modal visibility
+  const [editingComponent, setEditingComponent] = useState<Component | null>(null); // The component being edited
+  const [optionValue, setOptionValue] = useState<string>(''); // The new value for the option
 
   useEffect(() => {
+    if (pageId) {
+      fetchPage(pageId);
+    }
     fetchComponents();
   }, [pageId]);
+  
+  const fetchPage = async (id: string) => {
+    const data = await getPageById(id);
+    if (data) {
+      setPage(data);
+      setNewPageName(data.name);  // Initialize newPageName with the current page name
+    }
+  };
 
+  const handleSavePageName = async () => {
+    if (page) {
+      const updatedPage = await updatePage(page._id, newPageName, page.width, page.height);
+      if (updatedPage) {
+        setPage(updatedPage);
+        setIsEditingPageName(false);
+      }
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setNewPageName(page?.name || '');
+    setIsEditingPageName(false);
+  };
   const fetchComponents = async () => {
     try {
       const data = await getComponents(pageId || '');
@@ -48,7 +86,7 @@ const PageEditor = () => {
 
     addComponentToWorkspace(newComponent);
   };
-// add components to workspace
+
   const addComponentToWorkspace = (component: Component) => {
     const offset = components.length * DEFAULT_POSITION_OFFSET;
     const newComponent = {
@@ -59,7 +97,7 @@ const PageEditor = () => {
     setComponents((prev) => [...prev, newComponent]);
     setChosenComponents((prev) => prev.filter((comp) => comp !== component));
   };
-//remove components from workspace
+
   const removeComponentFromWorkspace = (id: string) => {
     const componentToRemove = components.find((comp) => comp.id === id);
     if (componentToRemove) {
@@ -84,71 +122,184 @@ const PageEditor = () => {
   const editComponentProperties = (component: Component) => {
     setChosenComponents([component]);
     setIsEditing(component.id || null); // Set the component as being edited
+    if (component.type === COMPONENT_TYPES.RADIO || component.type === COMPONENT_TYPES.CHECKBOX || component.type === COMPONENT_TYPES.SELECT) {
+      setOptionValue(component.properties.selected || '');
+      setShowModal(true);
+    }
   };
 
-  const saveComponentProperties = (component: Component, content: string) => {
-    setComponents((prev) =>
-      prev.map((comp) =>
-        comp.id === component.id ? { ...comp, properties: { ...comp.properties, content } } : comp
-      )
-    );
-    setChosenComponents((prev) => prev.filter((comp) => comp.id !== component.id)); // Remove from chosen list
+  // Update the saveComponentProperties function
+const saveComponentProperties = (component: Component, content: string) => {
+  setComponents((prev) =>
+    prev.map((comp) =>
+      comp.id === component.id
+        ? {
+            ...comp,
+            properties: {
+              ...comp.properties,
+              content: content, // This is the general property we're editing (for text-based components)
+              selected: component.type === COMPONENT_TYPES.RADIO || component.type === COMPONENT_TYPES.SELECT
+                ? content // For select and radio, we store the selected option
+                : component.properties.selected,
+              checked: component.type === COMPONENT_TYPES.CHECKBOX
+                ? content // For checkboxes, we handle checked status
+                : component.properties.checked,
+            },
+          }
+        : comp
+    )
+  );
+  setChosenComponents((prev) => prev.filter((comp) => comp.id !== component.id)); // Remove from chosen list
+  setIsEditing(null); // Hide the form after saving
+  setShowModal(false); // Hide the modal after saving
+};
 
-    setIsEditing(null); // Hide the form after saving
-  };
-
-  const renderComponent = (component: Component) => {
-    switch (component.type) {
-      case COMPONENT_TYPES.BUTTON:
-        return <button style={{ width: '100%', height: '100%' }}>{component.properties.content || 'Button'}</button>;
+const renderComponent = (component: Component) => {
+  switch (component.type) {
+    case COMPONENT_TYPES.BUTTON:
+      return <button style={{ width: '100%', height: '100%' }}>{component.properties.content || 'Button'}</button>;
       case COMPONENT_TYPES.RADIO:
         return (
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             <label>
-              <input type="radio" name="radio" /> Option 1
+              <input
+                type="radio"
+                name={component.id} // Make sure name is unique for each component
+                checked={component.properties.selected === 'Option 1'}
+                onChange={() => saveComponentProperties(component, 'Option 1')}
+              />
+              Option 1
             </label>
             <label>
-              <input type="radio" name="radio" /> Option 2
+              <input
+                type="radio"
+                name={component.id} // Same as above
+                checked={component.properties.selected === 'Option 2'}
+                onChange={() => saveComponentProperties(component, 'Option 2')}
+              />
+              Option 2
             </label>
           </div>
         );
-      case COMPONENT_TYPES.CHECKBOX:
-        return (
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <label>
-              <input type="checkbox" /> Check 1
-            </label>
-            <label>
-              <input type="checkbox" /> Check 2
-            </label>
-          </div>
-        );
-      case COMPONENT_TYPES.HEADER:
-        return <h1>{component.properties.content || 'Header'}</h1>;
-      case COMPONENT_TYPES.LINK:
-        return <a href={component.properties.href || '#'}>{component.properties.content || 'Link'}</a>;
-      case COMPONENT_TYPES.CONTAINER:
-        return <div style={{ backgroundColor: '#f0f0f0', padding: '10px' }}>{component.properties.content || 'Container'}</div>;
-      case COMPONENT_TYPES.PARAGRAPH:
-        return <p>{component.properties.content || 'Paragraph'}</p>;
+        case COMPONENT_TYPES.CHECKBOX:
+          return (
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={component.properties.checked === 'Check 1'} // Modify logic as needed
+                  onChange={() => saveComponentProperties(component, 'Check 1')}
+                />
+                Check 1
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={component.properties.checked === 'Check 2'}
+                  onChange={() => saveComponentProperties(component, 'Check 2')}
+                />
+                Check 2
+              </label>
+            </div>
+          );
+    case COMPONENT_TYPES.HEADER:
+      return <h1>{component.properties.content || 'Header'}</h1>;
+    case COMPONENT_TYPES.LINK:
+      return <a href={component.properties.href || '#'}>{component.properties.content || 'Link'}</a>;
+    case COMPONENT_TYPES.CONTAINER:
+      return <div style={{ backgroundColor: '#f0f0f0', padding: '10px' }}>{component.properties.content || 'Container'}</div>;
+    case COMPONENT_TYPES.PARAGRAPH:
+      return <p>{component.properties.content || 'Paragraph'}</p>;
       case COMPONENT_TYPES.SELECT:
         return (
-          <select style={{ width: '100%', height: '100%' }}>
+          <select
+            style={{ width: '100%', height: '100%' }}
+            value={component.properties.selected || ''}
+            onChange={(e) => saveComponentProperties(component, e.target.value)}
+          >
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
           </select>
         );
-      case COMPONENT_TYPES.TEXTBOX:
-        return <input type="text" value={component.properties.content || ''} style={{ width: '100%', height: '100%' }} />;
-      default:
-        return <div style={{ color: 'red' }}>Component type not recognized</div>;
-    }
-  };
-
+    case COMPONENT_TYPES.TEXTBOX:
+      return <input type="text" value={component.properties.content || ''} style={{ width: '100%', height: '100%' }} />;
+    default:
+      return <div style={{ color: 'red' }}>Component type not recognized</div>;
+  }
+};
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <button
+        onClick={() => navigate(`/project/${pageId || 'undefined'}`)}
+        style={{
+          backgroundColor: 'transparent',
+          border: 'none',
+          color: 'black',
+          cursor: 'pointer',
+          padding: '0',
+          marginBottom: '10px',
+          fontSize: '16px',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          width: 'auto',
+        }}
+        title="Back to Project Page"
+      >
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </button>
       <h2 style={{ color: '#4CAF50' }}>Interactive Page Editor</h2>
-
+       {/* Page Name Editing Section */}
+       {isEditingPageName ? (
+        <div>
+          <input
+            type="text"
+            value={newPageName}
+            onChange={(e) => setNewPageName(e.target.value)}
+            placeholder="Edit page name"
+          />
+          <button onClick={handleSavePageName}>Save</button>
+          <button onClick={handleCancelEdit}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{page?.name}</h3>
+          <button onClick={() => setIsEditingPageName(true)}>Edit Name</button>
+        </div>
+      )}
+        {/* Modal for editing radio/select/checkbox options */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Option</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="editOption">
+              <Form.Label>Edit Option</Form.Label>
+              <Form.Control
+                type="text"
+                value={optionValue}
+                onChange={(e) => setOptionValue(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              if (editingComponent) {
+                saveComponentProperties(editingComponent, optionValue);
+              }
+              setShowModal(false);
+            }}
+          >
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div>
         <label htmlFor="componentType">Choose Component: </label>
         <select
@@ -276,7 +427,7 @@ const PageEditor = () => {
                 )
               );
             }}
-            bounds="parent" // Ensures dragging is within the workspace
+            bounds="parent"
             enableResizing={{
               top: true,
               right: true,
@@ -288,10 +439,10 @@ const PageEditor = () => {
               bottomRight: true,
             }}
             style={{
-              margin: '0', // Ensure no margins are applied
-              padding: '0', // Remove any padding around the component
-              border: 'none', // Optional: Remove any borders around the component
-              boxSizing: 'border-box', // Make sure the padding and borders do not affect sizing
+              margin: '0',
+              padding: '0',
+              border: 'none',
+              boxSizing: 'border-box',
             }}
           >
             <div style={{ background: '#f0f0f0', width: '100%', height: '100%' }}>
