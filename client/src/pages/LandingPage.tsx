1d79232463ebe99d2eae02f06a04d8ea6ede1639
Flahles <joshuareslick@gmail.com>
2025-01-29 23:27:33 -0500

    Added Delete button for pages Delete for pages deletes all components Added Delete button for projects Delete for projects deletes all pages and components Updated add project button and added modal Updated add page button and added modal Added back button from projectpage to landingpage

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,27 +1,29 @@
 import ProjectCard from "../components/projectCard";
 import { getProjects, getUserProjects } from "../api/projects";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Auth from "../utils/auth.js";
 import { createProject } from "../api/projects";
+import { getMe } from "../utils/api";
+import { Modal, Button } from "react-bootstrap";
 
 import { useEffect, useState } from "react";
 
-interface Project {
-    _id: string;
-    projectName: string;
-    projectId: string;
-    image: string;
-    name: string;
-    author: string;
-}
-/*
-interface User {
-    _id: string;
-    username: string;
-    email: string;
-}*/
-
 const LandingPage = () => {
+
+    interface Project {
+        _id: string;
+        projectName: string;
+        projectId: string;
+        image: string;
+        name: string;
+        author: string;
+    }
+    
+    interface User {
+        _id: string;
+        username: string;
+        email: string;
+    }
 
     const navigate = useNavigate();
     //const projects: Project[] = []
@@ -29,6 +31,7 @@ const LandingPage = () => {
     const [ projects, setProjects ] = useState<Project[]>([]);
 
     const loggedIn: boolean = Auth.loggedIn();
+    const [ childDeleted, setChildDeleted ] = useState(false);
 
     useEffect(() => {
         const projectFiller = async () => {
@@ -42,11 +45,26 @@ const LandingPage = () => {
         }
 
         projectFiller();
-    }, []);
+    }, [childDeleted]);
 
     console.log("projects:", projects);
 
     if (loggedIn) {
+        const [ user, setUser ] = useState<User | null>(null);
+        const [ showModal, setShowModal ] = useState(false);
+
+        useEffect(() => {
+            const userFiller = async () => {
+                const data:User = await getMe();
+                setUser(data);
+            }
+
+            userFiller();
+        }, []);
+
+        const handleDelete = () => {
+            setChildDeleted(true);
+        }
 
         // Mock data for user
         /*const user: User = {
@@ -64,31 +82,65 @@ const LandingPage = () => {
             projects.push({ _id: projectId, projectName: projectName, projectId: projectId, name: projectName, image: projectImage, author: '' });
         }*/
         const projectsComponent = projects.map((project, index) => (
-            <Link to={`/project/${project.projectId}`} key={index}>
-                <ProjectCard key={index} project={project} />
-            </Link>
+            <ProjectCard loggedIn={loggedIn} key={index} onDelete={handleDelete} project={project} />
         ))
+
+        const nameProject = async () => {
+            const projectNameEl = document.getElementById("createProjectTextBox") as HTMLInputElement;
+            const projectSpanEl = document.getElementById("createProjectSpan") as HTMLSpanElement;
+            
+            if (!projectNameEl || !projectSpanEl) {
+                return;
+            }
+            const projectName = projectNameEl.value;
+
+            if (projectName) {
+                const project = await createProject(projectName);
+                if (project) {
+                    navigate(`/project/${project.projectId}`);
+                }
+            } else {
+                projectSpanEl.innerText = "Please enter a project name!";
+            }
+        }
 
         return (
             <>
                 <div id="projects" className="text-light p-5">
-                    <h1 id="projectsTitle">Welcome back, insertusername!</h1>
+                    <h1 id="projectsTitle">Welcome back, {user?.username}!</h1>
                     <div className="projectsContainer">
                         {projects.length > 0 ? projectsComponent : <p>No projects found, please click 
-                            <button onClick={ async () => {
-                                const projectName = prompt('Enter the name of your project');
+                            <a id="projectCreate" onClick={ async (e) => {
+                                e.preventDefault();
 
-                                if (projectName) {
-                                    const project = await createProject(projectName);
-                                    console.log("project: ", project);
-                                    if (project) {
-                                        navigate(`/project/${project.projectId}`);
-                                    }
-                                }
-                            }}>here</button> to create one!</p>}
+                                setShowModal(true);
+                            }}> here</a> to create one!</p>}
                         {}
                     </div>
                 </div>
+                <Modal
+                    show={showModal}
+                    onHide={() => setShowModal(false)}
+                    aria-labelledby='create-project-modal'>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Create Project</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <div>
+                            <span id="createProjectSpan"></span>
+                        </div>
+                        <div>
+                            <label htmlFor="createProjectTextBox">Project Name: </label>
+                            <input type="text" id="createProjectTextBox" name="createProjectTextBox"/>
+                        </div>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={()=> setShowModal(false)}>Cancel</Button>
+                        <Button variant="primary" onClick={() => nameProject()}>Create +</Button>
+                    </Modal.Footer>
+                </Modal>
             </>
         );
     } else {
@@ -108,7 +160,7 @@ const LandingPage = () => {
             <h1 id="projectsTitle">Community Projects</h1>
             <div className="projectsContainer">
                 {projects.map((project, index) => (
-                    <ProjectCard key={index} project={project} />
+                    <ProjectCard loggedIn={loggedIn} key={index} project={project} />
                 ))}
             </div>
         </div>
